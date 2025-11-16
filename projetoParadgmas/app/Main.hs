9030e0fa488c1587654d8ae71df3cde428391c59
pedrohasatos <afonsovich@gmail.com>
2025-11-16 17:37:09 -0300

    Verificar CPF/Email feito (sem acesso ao stack)

--- projetoParadgmas/app/Main.hs
+++ projetoParadgmas/app/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
-import Lib (mostrarMenuConversao, executarOpcaoConversao)
-import System.IO (hFlush, stdout) -- Comando flush para garantir a organixzação do I/O no terminal
+import Lib (mostrarMenuConversao, executarOpcaoConversao, mostrarMenuValidacao, executarOpcaoValidacao)
+import System.IO (hFlush, stdout)
 
 -- Menu Principal
 mostrarMenu :: IO ()
@@ -9,46 +9,64 @@ mostrarMenu = do
     putStrLn "\n--- MENU DE OPERAÇÃO ---"
     putStrLn "1. Transformação Monetária"
     putStrLn "2. Estatísticas"
-    putStrLn "3. Verificar e-mail ou cpf"
+    putStrLn "3. Verificar E-mail ou CPF"
     putStrLn "4. Sair"
     putStrLn "-------------------------"
     putStr "Escolha uma opção (1-4): "
-    hFlush stdout 
+    hFlush stdout
 
 -- I/O: Executar Opção
 executarOpcao :: Int -> IO ()
 executarOpcao 4 = putStrLn "Saindo do programa. Até mais!"
-executarOpcao opcao = 
+executarOpcao opcao =
     case opcao of
         1 -> do
             loopConversao
         2 -> putStrLn "Opção 2 (Estatísticas) selecionada. Implementação pendente."
-        3 -> putStrLn "Opção 3 (Verificar e-mail/cpf) selecionada. Implementação pendente."
+        3 -> do
+            loopValidacao  -- Chamada para o novo loop de validação
         _ -> putStrLn "Opção inválida. Por favor, escolha 1, 2, 3 ou 4."
 
-
+-- Loop para Conversão Monetária
 loopConversao :: IO ()
 loopConversao = do
     mostrarMenuConversao
     opcaoStr <- getLine
-    
+
+    -- Tratamento de entrada vazia e leitura
     let opcao = if null opcaoStr then 0 else read opcaoStr :: Int
-    
-    if opcao == 5 
+
+    if opcao == 5
         then putStrLn "Voltando ao Menu Principal..."
         else do
             executarOpcaoConversao opcao
             loopConversao
+
+-- Loop para Validação (CPF/E-mail)
+loopValidacao :: IO ()
+loopValidacao = do
+    mostrarMenuValidacao
+    opcaoStr <- getLine
+
+    -- Tratamento de entrada vazia e leitura
+    let opcao = if null opcaoStr then 0 else read opcaoStr :: Int
+
+    if opcao == 3
+        then putStrLn "Voltando ao Menu Principal..."
+        else do
+            executarOpcaoValidacao opcao
+            loopValidacao
 
 -- I/O: Loop Principal
 loop :: IO ()
 loop = do
     mostrarMenu
     opcaoStr <- getLine
-    
+
+    -- Tratamento de entrada vazia e leitura
     let opcao = if null opcaoStr then 0 else read opcaoStr :: Int
-    
-    if opcao == 4 
+
+    if opcao == 4
         then executarOpcao 4
         else do
             executarOpcao opcao
